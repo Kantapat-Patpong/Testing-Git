7a5922230ed3f3ee09d7a22b62b64fed6632e00e
กันตพัฒน์ แพทย์พงศ์ <kpongpat2557@gmail.com>
2024-03-06 15:05:51 +0700

    5 testing

--- practice_1/test_number_utils.py
+++ practice_1/test_number_utils.py
@@ -11,3 +11,23 @@ class PrimeListTest(unittest.TestCase):
         prime_list = [41,53,73,97]
         is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
+        
+    def test_give_143_277_337_533_is_prime(self):
+        prime_list = [143,277,337,533]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+        
+    def test_give_52_12_4_33_is_prime(self):
+        prime_list = [52,12,4,33]
+        isnot_prime = is_prime_list(prime_list)
+        self.assertFalse(isnot_prime)
+        
+    def test_give_613_437_469_799_is_prime(self):
+        prime_list = [613_437_469_799]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+        
+    def test_give_negative_767_629_441_13_is_prime(self):
+        prime_list = [-767,-629,-441,-13]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
